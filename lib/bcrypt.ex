b21525c468c48ee1fda26042b474b0e9abc41e73
Marian Bäuerle <m.baeuerle@semlabs.de>
2017-07-31 16:34:39 +0200

    Add empty default options

--- lib/bcrypt.ex
+++ lib/bcrypt.ex
@@ -105,7 +105,7 @@ defmodule Bcrypt do
   in order to make it more difficult for an attacker to identify users
   by timing responses.
   """
-  def no_user_verify(opts) do
+  def no_user_verify(opts \\ []) do
     hash_pwd_salt("password", opts)
     false
   end
